ea70defe8b37d2dd33ee360936e5802857b7179d
scazan <scott@teenage.engineering>
2020-04-18 08:17:23 +0200

    updating install script

--- install.sh
+++ install.sh
@@ -4,4 +4,9 @@ ln -s ~/dev/vimrc/.vimrc ~/.vimrc
 
 mkdir ~/.vimswap && mkdir ~/.vimundo && touch ~/.vimrc.config
 
-vim -c BundleInstall -c qall
+curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
+cp ./other/init.vim /home/scott/.config/nvim/init.vim
+
+nvim -c BundleInstall -c qall
